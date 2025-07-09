d0336112ec78de8377797e2bc2098ede2aaa80e9
Rodrigo Gomez <rodrigo.gomez.p@utec.edu.pe>
2025-07-08 22:15:02 -0500

    Update nn_optimizer.h

--- nn_optimizer.h
+++ nn_optimizer.h
@@ -8,31 +8,25 @@
 #pragma once
 
 #include "nn_interfaces.h"
+#include <cmath>
 
 namespace utec::neural_network {
 
     template <typename T>
-    class SGD : public IOptimizer<T> {
+    class Adam {
     private:
         T _lr;
-    public:
-        explicit SGD(T learning_rate = 0.01) : _lr(learning_rate) {}
-        void update(Tensor<T,2>& params, const Tensor<T,2>& grads) override {
-            params = params - (grads * _lr);
-        }
-    };
+        T _beta1, _beta2, _eps;
+        Tensor2D<T> _m, _v;
+        int _t = 0;
 
-    template <typename T>
-    class Adam : public IOptimizer<T> {
-    private:
-        T _lr, _beta1, _beta2, _eps;
-        Tensor<T, 2> _m, _v;
-        int _t;
     public:
         Adam(T lr = 0.001, T beta1 = 0.9, T beta2 = 0.999, T eps = 1e-8)
-            : _lr(lr), _beta1(beta1), _beta2(beta2), _eps(eps), _t(0) {}
+            : _lr(lr), _beta1(beta1), _beta2(beta2), _eps(eps) {}
 
-        void update(Tensor<T, 2>& params, const Tensor<T, 2>& grads) override {
+        void set_learning_rate(T lr) { _lr = lr; }
+
+        void update(Tensor2D<T>& params, const Tensor2D<T>& grads) {
             if (_t == 0) {
                 _m = Tensor2D<T>(params.shape());
                 _v = Tensor2D<T>(params.shape());
@@ -41,13 +35,13 @@ namespace utec::neural_network {
             }
             _t++;
 
-            _m = _m * _beta1 + grads * (1 - _beta1);
-            _v = _v * _beta2 + (grads*grads) * (1 - _beta2);
+            _m = (_m * _beta1) + (grads * (1.0 - _beta1));
+            _v = (_v * _beta2) + ((grads * grads) * (1.0 - _beta2));
 
-            auto m_hat = _m * (1.0 / (1.0 - std::pow(_beta1, _t)));
-            auto v_hat = _v * (1.0 / (1.0 - std::pow(_beta2, _t)));
+            Tensor2D<T> m_hat = _m / (1.0 - std::pow(_beta1, _t));
+            Tensor2D<T> v_hat = _v / (1.0 - std::pow(_beta2, _t));
 
-            for(size_t i=0; i < params.size(); ++i) {
+            for(size_t i = 0; i < params.size(); ++i) {
                 params[i] -= _lr * m_hat[i] / (std::sqrt(v_hat[i]) + _eps);
             }
         }
